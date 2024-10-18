26102ff7f99e85386c45f6125f0d930eec54e95d
Grace-3908254 <choore.11@gmail.com>
2024-10-18 00:42:39 -0400

    set up component import to page file, minor revision of spelling error of BillDetail.scss

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,16 @@ import './App.scss';
 import Temp from './components/ConfirmModal/Temp';
 import PieChartWithCenterLabel from "./components/PieDemo";
 
+import PaymentPage from './pages/PaymentPage'
+
 function App() {
   return <>
     {/* <PieChartWithCenterLabel /> */}
     <Temp />
+    <PaymentPage />
   </>;
 }
+
+//PaymentPage is only temporaryly placed here for visual refernce while styling
 
 export default App;
--- src/pages/PaymentPage/PaymentPage.jsx
+++ src/pages/PaymentPage/PaymentPage.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import './PaymentPage.scss'
 
+import Header from '../../components/PaymentPage/Header.jsx'
+import Brief from '../../components/PaymentPage/Brief.jsx'
+import BillDetail from '../../components/PaymentPage/BillDetail.jsx'
+
 export default function PaymentPage() {
   return (
     <div>
-
+      <Header />
+      <Brief />
+      <BillDetail />
     </div>
   )
 }
