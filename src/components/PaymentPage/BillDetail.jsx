d0a43914b7ef2617244d06a2eeafda6ced1a33ae
Grace-3908254 <choore.11@gmail.com>
2024-10-18 02:35:28 -0400

    style BillDetail component forthe page (not responsive; logic not set yet)

--- src/components/PaymentPage/BillDetail.jsx
+++ src/components/PaymentPage/BillDetail.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import './BillDetail.scss'
+import { useState } from 'react';
+
 
 export default function BillDetail() {
+
   return (
     <div className='bill'>
       <div className='bill__1'>
@@ -15,37 +18,37 @@ export default function BillDetail() {
             <p className='bill__amount'>$100</p>
         </div>
 
-        <div className='bill__3 bill-withAmount'>
-            <div className='bill-taxes'>
-                <p className='bill-taxes-icon'>+</p>
-                <p className='bill-taxes-word'>Taxes</p>  
+        <div className='bill__3 bill__withAmount'>
+            <div className='bill__taxes'>
+                <p className='bill__taxes-icon'>+</p>
+                <p className='bill__taxes-word'>Taxes</p>  
             </div>
             <p className='bill__amount'>$13</p>
         </div>
         
-        <div className='bill-4 bill-withAmount'>
-           <p>Remaining balance of previous bill(s)</p> 
+        <div className='bill__4 bill__withAmount'>
+           <p className='bill__remaining-balance'>Remaining balance of previous bill(s)</p> 
            <p className='bill__amount'>$0</p>
         </div>
         
-        <div className='bill-5 bill-withAmount'>
-            <p>Your total</p>
+        <div className='bill__5 bill__withAmount'>
+            <p className='bill__total'>Your total</p>
             <p className='bill__amount'>$113</p>
         </div>
 
-        <div className='bill-6 bill-withAmount'>
-            <p>Amount due</p>
+        <div className='bill__6 bill__withAmount'>
+            <p className='bill__amount-due'>Amount due</p>
             <p className='bill__amount'>$113</p>
         </div>
 
-        <div className='bill-7'>
-            <p className='bill-title'>Previous bill summary</p>
-            <p className='bill-date'>(Jan. 1, 5050)</p>
+        <div className='bill__7'>
+            <p className='bill__title'>Previous bill summary</p>
+            <p className='bill__date'>(Jan. 1, 5050)</p>
         </div>
 
-        <div className='bill-8 bill-withAmount'>
-            <p>Previous bill total</p>
-            <p className='bill__amount'>$100</p>
+        <div className='bill__8 bill__withAmount'>
+            <p className='bill__previous-bill'>Previous bill total</p>
+            <p className='bill__amount'></p>
         </div>
     </div>
   )
