f39871697d3910f0deefa3e19289731618105887
Alexandra Kononova <akononova93@gmail.com>
2024-10-18 09:52:01 -0400

    Add initiative list to the game page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import './App.scss';
+import "./App.scss";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import PieChartWithCenterLabel from "./components/PieDemo";
+import GamePage from "./pages/Game/Game";
 
 function App() {
-  return <>
-    <PieChartWithCenterLabel />
-  </>;
+  return (
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<PieChartWithCenterLabel />} />
+          <Route path="/game" element={<GamePage />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
