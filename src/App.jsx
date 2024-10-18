44bf1883ebc9b6012f9d2545063b4fd2904a997d
Grace-3908254 <choore.11@gmail.com>
2024-10-18 03:12:06 -0400

    set up route paths (external site is not added yet)

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
 import './App.scss';
 import Temp from './components/ConfirmModal/Temp';
 import PieChartWithCenterLabel from "./components/PieDemo";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import DashBoard from './pages/DashBoard/DashBoard.jsx'
+import ConfirmModal from './components/ConfirmModal/ConfirmModal.jsx'
+import PaymenPage from './pages/PaymentPage/PaymentPage.jsx'
 
 function App() {
-  return <>
-    {/* <PieChartWithCenterLabel /> */}
-    <Temp />
-  </>;
+  return (
+    <>
+      {/* <PieChartWithCenterLabel /> */}
+      {/* <Temp /> */}
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<DashBoard />}></Route>
+          <Route path='/ConfirmModal' element={<ConfirmModal />} />
+          <Route path='/PaymentPage' element={<PaymenPage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
 }
+
+//ConfirmModal -> Pay Now (make that into a Link tag) from PaymentPage
+//PaymentPage -> View bill (make that into a Link tag) from DashBoard
 
 export default App;
